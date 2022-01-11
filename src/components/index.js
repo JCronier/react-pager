3c4cfdd17fbb5a9ebd7a5aa038e36282687b377f
Jordan Cronier <jcronier777@gmail.com>
2022-01-11 10:47:05 -0800

    Set up components folder for export

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+/**Entry point for components folder
+ * 
+ * Import components in here
+ * 
+ * Example:
+ * 
+ * export {default as Login} from './Login';
+ * export {default as Register} from './Register';
+ * 
+ * Import components in App.js example:
+ * 
+ * import { Login, Register } from './components';
+ * 
+ */
