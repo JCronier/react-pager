04ef04b243736941af0604d18a36bb36ba340172
myron <myrony@sfu.ca>
2022-01-11 18:27:06 -0800

    button modification

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css'
 import Header from './components/Header'
 import Login from './components/Login'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 
 function App() {
-  //add users state
-  const [users,setUsers]=useState([])
-  //add user 
+  //logins state
+  const [logins,setLogins]=useState(['myron_yung@netflare.dev'])
+
+  //login page
+  const [showLogin,setShowLogin]=useState(true)
+
+   //check if user is registered  
   const verifyUser = (user)=>{
-    console.log(user)
+    //logins.filter((login)=>login<user).length>0? setShowLogin(false):alert('Not a registered user')
+    logins.includes(user)? setShowLogin(false):alert('Not a registered user')
+
   }
+
   return (
     <div className='container'>
       <Header title='Netflare'/>
-      <Login verify={verifyUser} />
+      {showLogin && <Login verifyUser={verifyUser}/>}
     </div>
   );
 }
