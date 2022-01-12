49471d941cdf69aeae59a7d86281ab4b135e2d56
Jordan Cronier <jcronier777@gmail.com>
2022-01-11 17:31:37 -0800

    Add some common api calls for app

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api_url = process.env.API_URL;
+const api_url = 'https://0gy9ldsaqj.execute-api.us-west-2.amazonaws.com/prod';
 
 const token = "";
 
@@ -10,9 +10,17 @@ const loginEndPoint = '/login';
 const emailEndPoint = '/email';
 
 const headers = {
-    'Content-Type': 'json',
-    'Authorization': token
+    'Content-Type': 'application/json',
+    'x-api-key': '097ubZybcb9ECVEQUTZ6M8caWPzBZXz7aHdFsqS7'
 };
+
+const body = {
+    "user": {
+        "username": "jcronier",
+        "name": "Jordan"
+    },
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Impjcm9uaWVyIiwiaWF0IjoxNjQxOTM5MzQyLCJleHAiOjE2NDE5NDI5NDJ9.3YW3UWDEwLukcdqk_WijAhB60Nw6QrQaINDm74yOu3Y"
+}
 
 /** Don't need authorization */
 
@@ -23,7 +31,7 @@ export const registerUser = (userData) => axios.post(api_url + registerEndPoint,
 export const loginUser = (userData) => axios.post(api_url + loginEndPoint, userData)
 
 // get all users
-export const getUsers = () => axios.get(api_url + usersEndpoint, { headers });
+export const getUsers = () => axios.get(api_url);
 
 // get a user
 export const getUser = (id) => axios.get(`${api_url}${usersEndpoint}/${id}`, { headers });
