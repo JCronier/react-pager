9525d5c9fcb245930569123032b2ab92d58478e8
myron <myrony@sfu.ca>
2022-01-12 21:45:10 -0800

    getUser method

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
-const api_url = 'https://0gy9ldsaqj.execute-api.us-west-2.amazonaws.com/prod';
+const api_url = 'https://ofwrz4w174.execute-api.us-west-2.amazonaws.com/backend';
 
 const token = "";
 
 const registerEndPoint = '/register';
 const usersEndpoint = '/users';
+const userEndpoint = '/users/user';
 const loginEndPoint = '/login';
 const emailEndPoint = '/email';
 
 const headers = {
     'Content-Type': 'application/json',
-    'x-api-key': '097ubZybcb9ECVEQUTZ6M8caWPzBZXz7aHdFsqS7'
+    'x-api-key': 'to4IMVwNqz3Btx3jOH9NLz289adm9xK9OwNgKb41'
 };
 
 const body = {
@@ -24,6 +25,16 @@ const body = {
 
 /** Don't need authorization */
 
+//getUsers function
+// const getLogins =()=>{
+//     getUsers().then((response)=>{
+//       const res = response.data
+//       //setLogins(res)
+//       console.log(res)
+//     }).catch((error)=>console.error(`Error: ${error}`))
+//   }
+//     getLogins()
+//   }
 // register a user 
 export const registerUser = (userData) => axios.post(api_url + registerEndPoint, userData);
 
@@ -31,17 +42,13 @@ export const registerUser = (userData) => axios.post(api_url + registerEndPoint,
 export const loginUser = (userData) => axios.post(api_url + loginEndPoint, userData)
 
 // get all users
-export const getUsers = () => axios.get(api_url);
+export const getUsers = () => axios.get(api_url + usersEndpoint);
 
 // get a user
-export const getUser = (id) => axios.get(`${api_url}${usersEndpoint}/${id}`, { headers });
-
-// create user
-export const createUser = (data) => axios.post(api_url + usersEndpoint, { headers, data});
+export const getUser = (email) => axios.get(`${api_url}${userEndpoint}?email=${email}`, { headers });
 
 // delete a user
-export const deleteUser = (id) => axios.delete(`${api_url}${usersEndpoint}/${id}`, { headers });
+export const deleteUser = (email) => axios.delete(`${api_url}${usersEndpoint}?email=${email}`, { headers });
 
 // send an email
 export const sendEmail = (data) => axios.post(api_url + emailEndPoint, { headers, data })
-
