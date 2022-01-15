48d752f7c340e922d8a2c4b318f1f6aa4a7574cd
myron <myrony@sfu.ca>
2022-01-14 16:33:11 -0800

    fixed routing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Loggedin from './components/Loggedin'
 import Register from './components/Register'
 import Home from './components/Home'
 import { getUsers } from './api'
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import {BrowserRouter as Router,Route,Routes,useNavigate} from "react-router-dom";
 import {Grid, Link,Typography} from "@mui/material"
 
 function App() {
@@ -25,31 +25,36 @@ function App() {
     ).catch((error)=>console.error(`Error: ${error}`))  
     },[])
     
+  let navigate = useNavigate()
    //check if login is registered  
   const verifyLogin = (login)=>{
     let res =logins.find((l)=>l.email===login)
     if(res){
       setShowLogin(false)
       setCurrLogin(res)
+      navigate('/loggedin')
     }else{
       alert('Not a registered user')
     }
   }
   return (
-    <Router>
-      <div style={{transform: `translate(40%,20%)`}}>
-        <Header title='Netflare'/>
-        <Grid container spacing={1} style={{marginTop: 10, marginBottom: 25}}>
-          <Grid item sm={1} ><Link href="/login" style={{ textDecoration: 'none'}}>login</Link></Grid>
-          <Grid item sm={1} ><Link href="/register" style={{ textDecoration: 'none' }}>register</Link></Grid>
-        </Grid>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path="/register" element={<Register />}/>
-          <Route path="/login" element={showLogin ? <Login verifyLogin={verifyLogin}/>:<Loggedin currLogin ={currLogin} users={logins} setUsers={setLogins}/>}/>
-        </Routes>
-      </div>
-    </Router>
+    <div style={{transform: `translate(40%,20%)`}}>
+      <Header title='Netflare'/>
+      {showLogin ? <Grid container spacing={1} style={{marginTop: 10, marginBottom: 25}}>
+        <Grid item sm={1} ><Link href="/login" style={{ textDecoration: 'none'}}>login</Link></Grid>
+        <Grid item sm={1} ><Link href="/register" style={{ textDecoration: 'none' }}>register</Link></Grid>
+      </Grid>:
+      <Grid container spacing={1} style={{marginTop: 10, marginBottom: 25}}>
+        <Grid item sm={1} ><Link href="/login" style={{ textDecoration: 'none'}}>LogOut</Link></Grid>
+      </Grid>
+      }
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/login" element={<Login verifyLogin={verifyLogin}/>}/>
+        <Route path="/loggedin" element={<Loggedin currLogin ={currLogin} users={logins} setUsers={setLogins}/>}/>
+      </Routes>
+    </div>
   );
 }
 
