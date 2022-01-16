03672a2cda05b63ad2d41100e5229c763a0b9faf
myron <myrony@sfu.ca>
2022-01-16 12:38:01 -0800

    styling for user page

--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,20 @@
 import {FaTimes} from 'react-icons/fa'
+import { Button, Typography,Grid, IconButton } from '@mui/material'
+import CancelIcon from '@mui/icons-material/Cancel'
 const User = ({user, onDelete, onUserClicked}) => {
   return (
         <div>
-            <h4 className='user'>
-                <button value={user.email} onClick={()=>onUserClicked(user)}>
-                    {user.name}
-                </button>
-                <FaTimes style={{color:'red', cursor:'crosshair'}} onClick={()=>onDelete(user)}/>
-            </h4>
+            <Grid container direction='row' alignItems='center'>
+                <Grid item >
+                    <Button variant='outlined' disableElevation  value={user.email} onClick={()=>onUserClicked(user)}>
+                    <Typography variant='h6' style={{textTransform:'none'}}>{user.name}</Typography>
+                    </Button>
+                </Grid>
+                <Grid item >
+                    <IconButton onClick={()=>onDelete(user)}><CancelIcon style={{color:'red', fontSize:'30'}} /></IconButton>
+                </Grid>
+            </Grid>
+            
         </div>
     )
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,8 +1,13 @@
+import { Grid } from '@mui/material'
 import User from './User'
 const Users = ({users, onDelete, currLogin, onUserClicked}) => {
     return (
         <div>
-            {users.map((user,index)=> user.email!==currLogin.email ? <User key={index} user={user} onDelete={onDelete} onUserClicked={onUserClicked}/>:null)}
+            <Grid container spacing={2}>
+                {users.map((user,index)=> user.email!==currLogin.email ? 
+                <Grid item xs={6} md={2} key={index}><User user={user} onDelete={onDelete} onUserClicked={onUserClicked}/>
+                </Grid>:null)}
+            </Grid>
         </div>
     )
 }
